459123b360754f12da6f477a6a832fc25acbd196
vasearar <prodius345@gmail.com>
2025-06-08 13:19:16 +0300

    +progress to footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import Logo from '../Logo'
 import Link from 'next/link'
+import Image from 'next/image'
+import Socials from './Socials'
 
 const Footer = () => {
   return (
-    <footer className='fixed w-screen h-screen grid auto-rows-min grid-cols-full py-8 bottom-0 -z-10 bg-forest-600 text-sand-50'>
-      <div className='col-span-5 flex flex-col'>
-        <Logo />
-        <h4 className='mt-8'>Nr. populației totale în cadrul componenței teritoriale din cele 14 localități ale raionelor Cimișlia și Căușeni. Nr. populației totale în cadrul componenței teritoriale din cele 14 localități ale raionelor Cimișlia și Căușeni...</h4>
-      </div>  
-        <div className='flex flex-col gap-2 col-start-1 col-span-3 row-start-2 mt-24'>
-          <h4 className='mb-4'>Detalii de contact</h4>
+    <footer className='fixed w-screen h-screen flex flex-col justify-between py-8 bottom-0 -z-10 bg-forest-600 text-sand-50'>
+      <div className='grid grid-cols-full auto-rows-min w-full'>
+        <div className='col-span-5 flex flex-col'>
+          <Logo />
+          <h4 className='mt-8'>Nr. populației totale în cadrul componenței teritoriale din cele 14 localități ale raionelor Cimișlia și Căușeni. Nr. populației totale în cadrul componenței teritoriale din cele 14 localități ale raionelor Cimișlia și Căușeni...</h4>
+        </div>  
+        <div className='flex flex-col gap-2 col-start-1 col-span-3 row-start-2'>
+          <h4 className='mb-4 font-bold'>Detalii de contact</h4>
           <p className='mb-4'>Adresa: str. 31 August 1989, s. Selemet, r. Cimișlia, Republica Moldova</p>
-          <p>Tel: 062 026 342</p>
-          <p>Fax: 024 184 285</p>
-          <p className='mt-4'>Email: info@stejaruldacilor.md</p>
+          <Link href="tel:37362026342">Tel: 062 026 342</Link>
+          <Link href="/">Fax: 024 184 285</Link>
+          <Link href="mailto:info@stejaruldacilor.md" className='mt-4'>Email: info@stejaruldacilor.md</Link>
         </div>
-        <div className='flex flex-col gap-2 col-start-4 col-span-2 row-start-2 mt-24'>
-          <h4 className='mb-2'>Link-uri de navigare</h4>
+        <div className='flex flex-col gap-2 col-start-4 col-span-2 row-start-2'>
+          <h4 className='mb-4 font-bold'>Link-uri de navigare</h4>
           <Link href="/">Acasă</Link>
           <Link href="/">Despre Noi</Link>
           <Link href="/">Anunțuri</Link>
@@ -25,7 +28,65 @@ const Footer = () => {
           <Link href="/">Istorii de succes</Link>
           <Link href="/">Contacte</Link>
         </div>
-        <div></div>
+      </div>
+      <div className='grid grid-cols-full auto-rows-min w-full'>
+        <div className='border-[1px] border-sand-50 opacity-25 col-span-full row-start-3 mb-2'></div>
+        <div className='row-start-4 col-span-full'>
+          <h4>Partenerii noștri:</h4>
+          <div className='flex justify-between relative items-center [&>*]:cursor-pointer [&>*]:transition [&>*]:hover:scale-110'>
+            <Image
+              src="/programul_leader.png"
+              alt="Programul Leader"
+              width={262}
+              height={64}
+            />
+            <Image
+              src="/ministerul_agriculturii_si_industriei_alimentare_al_republicii_moldova.png"
+              alt="Ministerul agriculturii și industriei alimentare al Republicii Moldova"
+              width={202}
+              height={129}
+            />
+            <Image
+              src="/aipa.png"
+              alt="Aipa"
+              width={200}
+              height={64}
+            />
+            <Image
+              src="/eu4moldova.png"
+              alt="EU4MOLDOVA"
+              width={127}
+              height={129}
+            />
+            <Image
+              src="/solidarity_fund_pl_in_moldova.png"
+              alt="Solidarity Fund PL in Moldova"
+              width={152}
+              height={129}
+            />
+          </div>
+        </div>
+        <div className='row-start-5 flex justify-between items-baseline col-span-full'>
+          <h4>© 2025 Grupul de Acțiune Locală Stejarul Dacilor </h4>
+          <Socials />
+          <div className='flex gap-8'>
+            <Link href="/">Termeni & Condiții</Link>
+            <Link
+              className='flex gap-1 items-center group justify-between' 
+              href="/">
+              Site by Studio Modvis
+              <svg width="10" height="10" 
+                className='group-hover:animate-spin transition ease-in' 
+                style={{ animationTimingFunction: "ease-in-out", animationDuration: "0.7s" }} 
+                viewBox="0 0 10 10" 
+                fill="none" 
+                xmlns="http://www.w3.org/2000/svg">
+                <path d="M10 4.99968C10 7.76126 7.76203 10 4.99998 10C2.23797 10 0 7.76189 0 4.99968C0 4.66771 0.0333576 4.34457 0.0950909 4.03151C2.55162 4.93293 4.97292 2.56128 4.10303 0.0800076C4.3928 0.0302512 4.69323 0 4.99935 0C7.76076 0 9.99936 2.24002 9.99936 5.00032L10 4.99968Z" fill="#FAFAFA"/>
+              </svg>
+            </Link>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
