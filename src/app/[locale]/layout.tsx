f490c338217d8458a3bb256705d24153eb12fe5d
vasearar <prodius345@gmail.com>
2025-08-12 17:33:21 +0300

    +scroll thin

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
 	const messages = await getMessages()
 
 	return (
-		<html lang={locale}>
+		<html lang={locale} className='scrollbars-75'>
 			<body className={`relative ${onest.className} bg-white`}>
 				<NextIntlClientProvider messages={messages}>
 					<SmoothScroll>
